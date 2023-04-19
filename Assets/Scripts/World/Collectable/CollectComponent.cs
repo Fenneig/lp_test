c12e92b8aa8fd89e4aa57f29b32776ccd685bd0a
Fenneig <ninetailedkot92@gmail.com>
2023-04-19 19:36:56 +0300

    polish collect component

--- Assets/Scripts/World/Collectable/CollectComponent.cs
+++ Assets/Scripts/World/Collectable/CollectComponent.cs
@@ -9,6 +9,10 @@ namespace LavaProject.World.Collectable
     public class CollectComponent : MonoBehaviour
     {
         [SerializeField] private ItemInfo _itemInfo;
+        [SerializeField] [Min(0)] 
+        [Tooltip("Time for flying away from target animation. If this equals zero object will immediately chase target.")] 
+        private float _flyAwayFromTargetTime;
+        [SerializeField] [Min(0)] private float _collectTime;
         private bool _isReadyToCollect;
         private Item _item;
 
@@ -23,15 +27,21 @@ namespace LavaProject.World.Collectable
         {
             if (!_isReadyToCollect) return;
             _isReadyToCollect = false;
-
             var directionToTarget = target.transform.position - transform.position;
             var firstPosition = transform.position - directionToTarget + Vector3.up;
 
-            Sequence collectItemSequence = DOTween.Sequence();
-            collectItemSequence
-                .Append(transform.DOMove(firstPosition, 1))
-                .OnComplete(() => { collectItemSequence.Kill(); })
-                .OnKill(() => StartCoroutine(MoveToTarget(transform.position, target.transform, 1)));
+            if (_flyAwayFromTargetTime != 0)
+            {
+                Sequence collectItemSequence = DOTween.Sequence();
+                collectItemSequence
+                    .Append(transform.DOMove(firstPosition, _flyAwayFromTargetTime))
+                    .OnComplete(() => { collectItemSequence.Kill(); })
+                    .OnKill(() => StartCoroutine(MoveToTarget(transform.position, target.transform, _collectTime)));
+            }
+            else
+            {
+                StartCoroutine(MoveToTarget(transform.position, target.transform, _collectTime));
+            }
         }
 
         private IEnumerator MoveToTarget(Vector3 startPosition, Transform targetTransform, float animationTime)
@@ -48,6 +58,7 @@ namespace LavaProject.World.Collectable
 
             OnCollectComplete();
         }
+
 
         public void PrepareToCollect()
         {
